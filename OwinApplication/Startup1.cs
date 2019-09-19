69f91fd54eb1c990aee905a6ea57cb4efb3ceacb
Michael Silliman <silliman@deadlinesolutions.com>
2019-09-19 15:12:18 -0400

    update

--- OwinApplication/Startup1.cs
+++ OwinApplication/Startup1.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Net.Http.Formatting;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using NSwag.AspNet.Owin;
 using Owin;
 
 namespace OwinApplication
@@ -14,8 +18,24 @@ namespace OwinApplication
 
             HttpConfiguration config = new HttpConfiguration();
             config.MapHttpAttributeRoutes();
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+
+
             app.UseWebApi(config);
 
+            app.UseSwaggerUi3(typeof(Startup).Assembly, settings => { });
+
+            string webPath = @"C:\Temp";
+
+            FileServerOptions fileServerOptions = new FileServerOptions()
+            {
+                RequestPath = PathString.Empty,
+                FileSystem = new PhysicalFileSystem(webPath),
+                EnableDirectoryBrowsing = true,
+            };
+
+            app.UseFileServer(fileServerOptions);
         }
     }
 }
