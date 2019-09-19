051dac198fb66fb0688a49f528c0f7f9ce11324b
Michael Silliman <silliman@deadlinesolutions.com>
2019-09-19 14:38:52 -0400

    initial add

---
+++ OwinApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinApplication
+{
+    class Program
+    {
+        private IDisposable _webApplication;
+
+        static void Main(string[] args)
+        {
+            string baseAddress = string.Format("http://*:9999");
+            WebApp.Start<Startup>(url: baseAddress);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ OwinApplication/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+namespace OwinApplication
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            app.UseWebApi(config);
+
+        }
+    }
+}
